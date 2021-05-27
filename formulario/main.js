576c063f390227eb8d04b675a190b3179f221d15
Formacion <formacion@FTAULAS.ORG>
2021-05-27 10:58:00 +0200

    ejercicios

---
+++ formulario/main.js
@@ -0,0 +1,15 @@
+/*funcion escita con arrow*/
+enviarAlerta = () => {
+    let dato = document.getElementsByName('texto')
+[0]. value;
+alert(dato);
+}
+
+/*función escritade forma tradicional*/
+function enviarAlerta(){
+    let dato= document.getElementsByTagName('texto')
+    [0].value;
+    alert(dato);
+
+
+}
